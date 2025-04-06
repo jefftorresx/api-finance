8fb0490f032147f795d4a0c01037d560248e9cbf
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-04-06 10:50:58 -0400

    Cria estrutura inicial do projeto via Spring Initializr + dependências do JWT

---
+++ src/main/java/com/jeff/api_finance/ApiDeFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.api_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeFinancasApplication.class, args);
+	}
+
+}
